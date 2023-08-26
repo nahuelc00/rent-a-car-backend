340d87b99e8e014fb505ed79ade7db984f7651e4
Nahuel <nahuel.lorenzodev@gmail.com>
2023-08-25 22:38:20 -0300

    complete index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-function main() {
+require('dotenv').config();
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const configureDI = require('./config/diContainer');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+function main() {
+  const container = configureDI();
+  const carController = container.get('CarController');
+  carController.setupRoutes(app);
+
+  app.listen(PORT || 8080, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
 }
 main();
